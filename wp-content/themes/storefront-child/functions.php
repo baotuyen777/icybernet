a393669ed0124bf2efb3176e51c11de5ca3124a5
David2 <baotuyen111@gmail.com>
2022-10-01 08:12:50 +0700

    doing dynamic home

--- wp-content/themes/storefront-child/functions.php
+++ wp-content/themes/storefront-child/functions.php
@@ -2,6 +2,7 @@
 
 require 'inc/cpt.php';
 require 'inc/ctx.php';
+require 'inc/icon.php';
 //remove_filter( 'the_content', 'wpautop' );
 //remove_filter( 'the_excerpt', 'wpautop' );
 //add_filter ( 'the_content', 'add_newlines_to_post_content' );
--- wp-content/themes/storefront-child/home.php
+++ wp-content/themes/storefront-child/home.php
@@ -408,51 +408,33 @@ get_header(); ?>
                     <a data-sqe="link" href="<?php echo the_permalink() ?>">
                         <div class="yZLQT4">
                             <div class="uA1waf _4QQ4Ir">
-                                <div class="n-CE6j iR+sxV">
-                                    <?php echo woocommerce_get_product_thumbnail() ?>
-                                    <div class="X5j9y1">
-                                        <div class="VGupoO wuWR8e"
-                                             style="color: rgb(242, 82, 32);">
-                                            <span class="_6UEPfo">Yêu thích</span>
-                                        </div>
+                                <?php echo woocommerce_get_product_thumbnail('full') ?>
+                                <div class="X5j9y1">
+                                    <div class="VGupoO wuWR8e"
+                                         style="color: rgb(242, 82, 32);">
+                                        <span class="_6UEPfo">Yêu thích</span>
                                     </div>
-                                    <div class="vmaKHl">
-                                        <div class="C2-vN- dCT7bq Od5TJM">
-                                            <span class="percent">31%</span><span class="mXP-A3">giảm</span>
-                                        </div>
-                                    </div>
-
                                 </div>
+                                <div class="vmaKHl">
+                                    <div class="C2-vN- dCT7bq Od5TJM">
+                                        <span class="percent">31%</span><span class="mXP-A3">giảm</span>
+                                    </div>
+                                </div>
+
                                 <div class="W3bJfG">
                                     <div class="qUEEG4">
                                         <div class="hPc1Pf">
                                             <div class="vc0PvV AxYdVM"><?php the_title() ?></div>
                                         </div>
                                         <div class="bVKmWS">
-                                            <div class="YGu43g">
-                                                <svg viewBox="0 0 20 12" height="12" width="20"
-                                                     class="freeship">
-                                                    <path fill="#00bfa5"
-                                                          d="M5 0h10c.6 0 1 .4 1 1v7c0 .6-.4 1-1 1H5c-.6 0-1-.4-1-1V1c0-.6.4-1 1-1z"></path>
-                                                    <path fill="#00bfa5"
-                                                          d="M14 2h5c.6 0 1 .4 1 1v5c0 .6-.4 1-1 1h-5c-.6 0-1-.4-1-1V3c0-.6.4-1 1-1z"></path>
-                                                    <circle fill="#00bfa5" cx="8" cy="10" r="2"></circle>
-                                                    <circle fill="#00bfa5" cx="15" cy="10" r="2"></circle>
-                                                    <path fill="#fff"
-                                                          d="M17.3 5.9h.7c-.2.6-.7.9-1.5.9-1 0-1.6-.6-1.6-1.6s.6-1.7 1.6-1.7 1.5.6 1.5 1.6v.2h-2.4c0 .6.4.9.9.9.4.1.7 0 .8-.3zm-.8-1.8c-.5 0-.9.3-.9.8h1.7c0-.5-.3-.8-.8-.8zm-2.7 1.8h.7c-.2.6-.7.9-1.5.9-1 0-1.6-.6-1.6-1.6S12 3.5 13 3.5s1.5.6 1.5 1.6v.2h-2.4c0 .6.4.9.9.9.4.1.7 0 .8-.3zM13 4.1c-.5 0-.9.3-.9.8h1.7c0-.5-.3-.8-.8-.8zM9.4 6.8V3.6h.6v.5h.1c.1-.3.5-.5.9-.5h.3v.6h-.4c-.5 0-.9.3-.9.8v1.9h-.6zm-2.7 0H6V2.6h2.8v.6H6.7v1.3h2V5h-2v1.8z"></path>
-                                                    <path fill="#00bfa5"
-                                                          d="M.5 8.5H4v.8H.5z"></path>
-                                                    <path fill="#00bfa5"
-                                                          d="M0 10.2h3.5v.8H0z"></path>
-                                                    <circle fill="#047565" cx="8" cy="10" r="1"></circle>
-                                                    <circle fill="#047565" cx="15" cy="10" r="1"></circle>
-                                                </svg>
-                                            </div>
+                                            <?php echo FREESHIP ?>
                                         </div>
                                     </div>
                                     <div class="imdVqB _2fuFWg">
-                                        <div class="WSVId4 fepoRf"><span class="j0vBz2">69.000</span></div>
-                                        <div class="upl8wJ _82UoSS">Đã bán 1,1k</div>
+                                        <div class="WSVId4 fepoRf"><span
+                                                    class="j0vBz2"><?php echo number_format($product->get_price())?></span>
+                                        </div>
+                                        <div class="upl8wJ _82UoSS">Đã bán <?php echo get_field('quantity_sold')?></div>
                                     </div>
                                 </div>
                                 <div class="shopee-item-card__hover-footer _1X2yZq">
